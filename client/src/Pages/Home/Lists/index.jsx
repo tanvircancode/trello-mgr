85bc11f500c4cf6c749de3ddfac3c6a0c852d976
tanvircancode <tanahmchw241996@gmail.com>
2024-07-28 22:54:22 +0600

    List Page desigh updated

--- client/src/Pages/Home/Lists/index.jsx
+++ client/src/Pages/Home/Lists/index.jsx
@@ -1,8 +1,7 @@
-import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BASE_URL } from "../../../config";
 import { BsPersonPlus, BsTrash3 } from "react-icons/bs";
 import "./lists.scss";
@@ -11,6 +10,11 @@ const List = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [listTitle, setListTitle] = useState("");
     const [showStageAction, setShowStageAction] = useState(false);
+    const [stageActionPosition, setStageActionPosition] = useState({
+        top: 0,
+        left: 0,
+    });
+
     const selectedProject = useSelector((state) => state.selectedProject);
 
     const userId = localStorage.getItem("user_id");
@@ -18,15 +22,21 @@ const List = () => {
     const blur = useSelector((state) => state.makeBlur);
     const token = useSelector((state) => state.token);
     const stages = useSelector((state) => state.stages);
-    console.log(stages);
+
     const dispatch = useDispatch();
 
     const cancelAddList = () => {
         setListTitle("");
     };
 
-    const handleStageAction = () => {
+    const handleStageAction = (event) => {
+        const rect = event.target.getBoundingClientRect();
+        setStageActionPosition({ top: rect.bottom + 10, left: rect.left });
         setShowStageAction(true);
+    };
+
+    const handleCloseStageAction = () => {
+        setShowStageAction(false);
     };
 
     const handleCreateList = async () => {
@@ -77,80 +87,104 @@ const List = () => {
     };
 
     return (
-        <div className="card-list d-flex flex-wrap gap-2">
-            <div className="d-flex gap-2">
-                {stages &&
-                    stages.length > 0 &&
-                    stages.map((stage, index) => {
-                        return (
-                            <div key={index} className={`card custom-card`}>
-                                <div className="card-body custom-stage-body  d-flex align-items-center">
-                                    <span className="card-title custom-stage-title m-0">
-                                        {stage && stage.title}
-                                    </span>
-                                    <span
-                                        className="stage-horizontal-dots mb-1"
-                                        onClick={handleStageAction}
-                                    >
-                                        ...
-                                    </span>
+        <div className="d-flex">
+            <div className="card-list d-flex gap-2">
+                <div className="d-flex gap-2">
+                    {stages &&
+                        stages.length > 0 &&
+                        stages.map((stage, index) => {
+                            return (
+                                <div key={index} className={`card custom-card`}>
+                                    <div className="card-body custom-stage-body d-flex justify-content-between align-items-center">
+                                        <span className="card-title custom-stage-title m-0">
+                                            {stage && stage.title}
+                                        </span>
+                                        <span
+                                            className="stage-horizontal-dots mb-1"
+                                            onClick={handleStageAction}
+                                        >
+                                            ...
+                                        </span>
+                                    </div>
                                 </div>
-                            </div>
-                        );
-                    })}
-            </div>
-
-            {handleStageAction && (
-                // <div>
-                <div className="card" style={{ width: "18rem" }}>
-                    <div className="d-flex cursor-pointer">
-                        <div className="card-header">Featured</div> <div>X</div>
-                    </div>
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">An item</li>
-                        <li className="list-group-item">A second item</li>
-                        <li className="list-group-item">A third item</li>
-                    </ul>
+                            );
+                        })}
                 </div>
-                // </div>
-            )}
 
-            <div className="custom-card-add">
-                <input
-                    type="text"
-                    className="form-control stage-title-input"
-                    value={listTitle}
-                    placeholder={
-                        stages.length > 0
-                            ? "+ Add another list"
-                            : "+ Add a list"
-                    }
-                    onChange={(e) => setListTitle(e.target.value)}
-                />
-                {listTitle && (
-                    <div className="d-flex align-items-center gap-3 mt-2 ">
-                        <button
-                            type="button"
-                            className="btn btn-primary create-card-button"
-                            disabled={isLoading}
-                            onClick={handleCreateList}
-                        >
-                            <span className="add-card-text">
-                                {isLoading ? "Loading..." : "Add List"}
+                {showStageAction && (
+                    <div
+                        className="card"
+                        style={{
+                            width: "18rem",
+                            position: "absolute",
+                            top: stageActionPosition.top,
+                            left: stageActionPosition.left,
+                            zIndex: 1000,
+                        }}
+                    >
+                        <div className="card-header d-flex justify-content-between">
+                            <span style={{ fontWeight: "600" }}>
+                                List Actions
                             </span>
-                        </button>
-                        <button
-                            type="button"
-                            className="btn-close"
-                            aria-label="Close"
-                            disabled={isLoading}
-                            style={{ fontSize: "12px" }}
-                            onClick={cancelAddList}
-                        ></button>
+                            <div
+                                style={{ cursor: "pointer" }}
+                                onClick={handleCloseStageAction}
+                            >
+                                x
+                            </div>
+                        </div>
+
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item stage-li-item">
+                                Add card
+                            </li>
+                            <li className="list-group-item stage-li-item">
+                                Copy list
+                            </li>
+                            <li className="list-group-item stage-li-item">
+                                Move list
+                            </li>
+                        </ul>
                     </div>
                 )}
+                <div className="custom-card-add">
+                    <input
+                        type="text"
+                        className="form-control stage-title-input"
+                        value={listTitle}
+                        placeholder={
+                            stages.length > 0
+                                ? "+ Add another list"
+                                : "+ Add a list"
+                        }
+                        onChange={(e) => setListTitle(e.target.value)}
+                    />
+                    {listTitle && (
+                        <div className="d-flex align-items-center gap-3 mt-2 ">
+                            <button
+                                type="button"
+                                className="btn btn-primary create-card-button"
+                                disabled={isLoading}
+                                onClick={handleCreateList}
+                            >
+                                <span className="add-card-text">
+                                    {isLoading ? "Loading..." : "Add List"}
+                                </span>
+                            </button>
+                            <button
+                                type="button"
+                                className="btn-close"
+                                aria-label="Close"
+                                disabled={isLoading}
+                                style={{ fontSize: "12px" }}
+                                onClick={cancelAddList}
+                            ></button>
+                        </div>
+                    )}
+                </div>
             </div>
-        </div>
+            
+         </div>
     );
 };
 
