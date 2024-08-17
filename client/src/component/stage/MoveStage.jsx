7d7d70d0cc4e23a12e0245e891a688d53ed5101a
tanvircancode <tanahmchw241996@gmail.com>
2024-08-17 22:49:26 +0600

    Updated Movestage

--- client/src/component/stage/MoveStage.jsx
+++ client/src/component/stage/MoveStage.jsx
@@ -3,7 +3,6 @@ import { BsArrowLeftShort } from "react-icons/bs";
 import { useDispatch, useSelector } from "react-redux";
 import { setShowMoveStage, setShowStageAction } from "../../store";
 const MoveStage = ({ showRect }) => {
-  
     const [stageActionPosition, setStageActionPosition] = useState({
         top: 0,
         left: 0,
@@ -14,11 +13,12 @@ const MoveStage = ({ showRect }) => {
     const showMoveStage = useSelector((state) => state.showMoveStage);
     const projects = useSelector((state) => state.projects);
     const selectedProject = useSelector((state) => state.selectedProject);
-    console.log(selectedProject);
+    console.log(projects);
 
-    const [selectedProjectFromOption, setSelectedProjectFromOption] = useState(null);
+    const [selectedProjectFromOption, setSelectedProjectFromOption] =
+        useState(null);
     const [positions, setPositions] = useState([]);
-    const [selectedPosition, setSelectedPosition] = useState('');
+    const [selectedPosition, setSelectedPosition] = useState("");
 
     const handleMoveStageClick = (showMoveStage, showStageAction) => {
         dispatch(setShowMoveStage({ showMoveStage }));
@@ -26,31 +26,39 @@ const MoveStage = ({ showRect }) => {
     };
 
     const handleProjectSelection = (e) => {
-        setSelectedProjectFromOption(e.target.value);
-        console.log(e.target.value);
-    }
+        const selectedProjectId = e.target.value;
+        const selectedProject = projects.find(
+            (project) => project.id == selectedProjectId
+        );
+
+        setSelectedProjectFromOption(selectedProject);
+        console.log(selectedProjectFromOption);
+    };
 
     useEffect(() => {
-      console.log(selectedProjectFromOption)
+        // const selectedProjectId = selectedProject.id;
+        setSelectedProjectFromOption(selectedProject);
+
         if (showRect && showRect.bottom && showRect.left) {
             setStageActionPosition({
                 top: showRect.bottom + 10,
                 left: showRect.left,
             });
         }
-    }, [showRect, selectedProjectFromOption]);
+    }, [showRect]);
 
     return (
         <div
             className="card"
             style={{
                 width: "18rem",
+                padding: '0 15px 10px',
                 position: "absolute",
                 top: stageActionPosition.top,
                 left: stageActionPosition.left,
                 zIndex: 1000,
             }}
-        >  
+        >
             <div className="card-header d-flex justify-content-between align-items-center">
                 <BsArrowLeftShort
                     onClick={() => handleMoveStageClick(false, true)}
@@ -65,36 +73,38 @@ const MoveStage = ({ showRect }) => {
                 </div>
             </div>
             <label htmlFor="board">Board</label>
-            <select className="form-select" 
-            onChange={handleProjectSelection}  
-            >
+            <select className="form-select" onChange={handleProjectSelection}>
                 {projects.map((project) => {
-                    return (    
-                        <option selected={selectedProject.id == project.id } key={project.id} value={project.id} >
-                            {project.title} {selectedProject.id == project.id && <span>(current)</span>}
-                        </option>  
-                    )
+                    return (
+                        <option
+                            selected={selectedProject.id == project.id}
+                            key={project.id}
+                            value={project.id}
+                        >
+                            {project.title}
+                            {selectedProject.id == project.id && (
+                                <span>(current)</span>
+                            )}
+                        </option>
+                    );
                 })}
-                
-            
             </select>
-            
+
             <br />
             <label htmlFor="board">Position</label>
 
-            
-            <select class="form-select" 
-            onChange={(e) => setSelectedProjectFromOption(e.target.value)}  
+            <select
+                class="form-select"
+                // onChange={(e) => setSelectedProjectFromOption(e.target.value)}
             >
-                {selectedProject.stages.map((stage) => {
-                    return (
-                        <option  key={stage.id} value={stage.id} >
-                            {stage.position} 
-                        </option>
-                    )
-                })}
-                
-            
+                {selectedProjectFromOption &&
+                    selectedProjectFromOption.stages.map((stage) => {
+                        return (
+                            <option selected={stage.position} key={stage.id} value={stage.id}>
+                                {stage.position} 
+                            </option>
+                        );
+                    })}
             </select>
         </div>
     );
